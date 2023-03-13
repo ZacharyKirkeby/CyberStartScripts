51652a6a60d48739fdff7872da26c72e1ac69194
Zachary Kirkeby <95650247+ZacharyKirkeby@users.noreply.github.com>
2023-03-13 10:26:55 -0500

    Update Communications.py

--- Communications.py
+++ Communications.py
@@ -7,15 +7,15 @@ def get_encrypted_messages(host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         s.sendall(b'GET')
-        data = s.recv(1024).decode()
+        data = s.recv(2048).decode()
         return data
 
 def send_decrypted_messages(host, port, messages):
     """connect to the server and send decrypted messages"""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
-        s.sendall('\n'.join(messages).encode())
-        data = s.recv(1024).decode()
+        s.sendall(messages.encode())
+        data = s.recv(2048).decode()
         return data
 
 def caesar_cipher(text, shift):
@@ -34,7 +34,7 @@ host = 'localhost'
 port = 10000
 encrypted_messages = get_encrypted_messages(host, port).split('\n')
 
-miniDictionary = [' OF ',' AND ',' OR ',' THE ',' MY ',' HER ',' I ',' FOR ',' IF ',' ARE ',' AN ',' THEY ',' BUT ',' SO ']
+miniDictionary = [' OF ',' AND ',' OR ',' BE ',' THE ',' MY ',' HER ',' I ',' FOR ',' IF ',' ARE ',' AN ',' THEY ',' BUT ',' SO ', ' YOU ',' TO ']
 
 def tryAll(phrasesList):
     for i in range(0,4):
@@ -43,17 +43,15 @@ def tryAll(phrasesList):
             for words in miniDictionary:
               	if words in message: 
                   encrypted_messages[i] = message
-                 # print(message)
-                 # print('^^^^^^^^^^^')
-                  
-        #print('***************************************')
+                 
   
 tryAll(encrypted_messages)
 encrypted_messages.pop(0)
 encrypted_messages.pop(3)
-print(encrypted_messages)
-response = send_decrypted_messages(host, port, encrypted_messages)
+#print(encrypted_messages)
+responseString = encrypted_messages[0] + '\n ' + encrypted_messages[1] + '\n ' + encrypted_messages[2]
+print(responseString)
+response = send_decrypted_messages(host, port, responseString)
 print(response)
-  
   
   
